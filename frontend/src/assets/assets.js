07f1562e52dc909cf9947adf4aa84faf1b1dc695
Murali Krishna <krishnamurali.barla@sasi.ac.in>
2025-02-03 15:30:23 +0530

    .

---
+++ frontend/src/assets/assets.js
@@ -0,0 +1,6 @@
+import chatspage_bg_image from "./gradient-bg-image.jpg";
+
+export const images = {
+  chatspage_bg_image
+};
+
--- frontend/src/pages/ChatsPage.jsx
+++ frontend/src/pages/ChatsPage.jsx
@@ -1,13 +1,28 @@
 import React from 'react'
+import {useNavigate } from 'react-router-dom';
+import { images } from '../assets/assets';
 
 const ChatsPage = () => {
+  const navigate = useNavigate();
+  const Logout = () => {
+    localStorage.removeItem('userInfo');
+    navigate("/");
+  }
   return (
-    <div>
-      chatspage
-
-
-
-      <button className='btn-danger'>Logout</button>
+    <div
+      style={{
+        backgroundImage: `url(${images.chatspage_bg_image})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        height: "100vh",
+        width: "100%",
+      }}
+    >
+      
+      <h2 className='text-white'>Chat Page</h2>
+      <button className="btn btn-danger" onClick={Logout}>
+        Logout
+      </button>
     </div>
   )
 }
